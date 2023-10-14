34262c657b9bf4d2da6b028917fca8d926da37c4
TMG <66220144+TMG8047KG@users.noreply.github.com>
2023-10-14 20:44:03 +0300

    Added Alert Messages (Warning and Info)

--- src/main/java/com/rosekingdom/rosekingdom/Core/CommandManager/CommandManager.java
+++ src/main/java/com/rosekingdom/rosekingdom/Core/CommandManager/CommandManager.java
@@ -5,6 +5,7 @@ import com.rosekingdom.rosekingdom.Core.CommandManager.FeatureTesting.CheckPermi
 import com.rosekingdom.rosekingdom.Core.CommandManager.FeatureTesting.Demoscreen;
 import com.rosekingdom.rosekingdom.Core.CommandManager.FeatureTesting.Profiles;
 import com.rosekingdom.rosekingdom.Core.CommandManager.FeatureTesting.ProtocolTest;
+import com.rosekingdom.rosekingdom.Core.Utils.Message;
 import com.rosekingdom.rosekingdom.Graves.GraveCommand;
 import com.rosekingdom.rosekingdom.Locations.CoordinatesBC;
 import com.rosekingdom.rosekingdom.Locations.Locations;
@@ -12,7 +13,6 @@ import com.rosekingdom.rosekingdom.Moderation.Bugs;
 import com.rosekingdom.rosekingdom.Moderation.Feedback;
 import com.rosekingdom.rosekingdom.Moderation.Rank;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -74,7 +74,7 @@ public class CommandManager implements TabExecutor {
             for (CommandRK rk : getCommands()) {
                 if(rk.getAliases().contains(label)) {
                     if(rk.getArgumentRequirement() && args.length==0){
-                        sender.sendMessage(Component.text("Missing Arguments!", TextColor.fromHexString("#e30000")));
+                        sender.sendMessage(Message.Warning("Missing Arguments!"));
                         return false;
                     }
                     if (args.length > 0 && rk.hasSubCommands()) {
--- src/main/java/com/rosekingdom/rosekingdom/Core/Utils/Message.java
+++ src/main/java/com/rosekingdom/rosekingdom/Core/Utils/Message.java
@@ -1,6 +1,7 @@
 package com.rosekingdom.rosekingdom.Core.Utils;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
 
 public class Message {
@@ -14,5 +15,16 @@ public class Message {
 
     public static void Global(String text){
         Bukkit.broadcast(Component.text(text));
+    }
+
+    public static Component Warning(String text){
+        return Component.text("\uEe01 ")
+                .append(Component.text(text))
+                .color(TextColor.fromHexString("#e30000"));
+    }
+    public static Component Info(String text){
+        return Component.text("\uEe02 ")
+                .append(Component.text(text))
+                .color(TextColor.fromHexString("#ebb22f"));
     }
 }
