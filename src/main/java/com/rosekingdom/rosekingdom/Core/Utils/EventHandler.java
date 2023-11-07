cc40e4739b918e27be0f14ba83df56a46f79984d
TMG <66220144+TMG8047KG@users.noreply.github.com>
2023-11-07 22:42:28 +0200

    Added Daily Streak Score

--- src/main/java/com/rosekingdom/rosekingdom/Core/Utils/EventHandler.java
+++ src/main/java/com/rosekingdom/rosekingdom/Core/Utils/EventHandler.java
@@ -4,7 +4,8 @@ import com.rosekingdom.rosekingdom.Core.Events.onChatEvent;
 import com.rosekingdom.rosekingdom.Core.Events.onJoin;
 import com.rosekingdom.rosekingdom.Core.Events.onLeave;
 import com.rosekingdom.rosekingdom.Graves.onDead;
-import com.rosekingdom.rosekingdom.Profiles.GUIhandler;
+import com.rosekingdom.rosekingdom.Profiles.Events.DailyStreakChecker;
+import com.rosekingdom.rosekingdom.Profiles.Events.GUIhandler;
 import org.bukkit.plugin.Plugin;
 
 import static org.bukkit.Bukkit.getServer;
@@ -16,5 +17,6 @@ public class EventHandler {
         getServer().getPluginManager().registerEvents(new onLeave(), plugin);
         getServer().getPluginManager().registerEvents(new onChatEvent(), plugin);
         getServer().getPluginManager().registerEvents(new GUIhandler(), plugin);
+        getServer().getPluginManager().registerEvents(new DailyStreakChecker(), plugin);
     }
 }
