6cecf5cb82cd7b94594cadcdd9e36ae46b10d554
TMG <66220144+TMG8047KG@users.noreply.github.com>
2023-11-07 23:22:20 +0200

    Conflict Merge and Version Change

--- src/main/java/com/rosekingdom/rosekingdom/Profiles/Items/activityIndicator.java
+++ src/main/java/com/rosekingdom/rosekingdom/Profiles/Items/activityIndicator.java
@@ -1,5 +1,7 @@
 package com.rosekingdom.rosekingdom.Profiles.Items;
 
+import com.rosekingdom.rosekingdom.Core.Utils.MillisToTime;
+import com.rosekingdom.rosekingdom.Profiles.Statements.ProfileStatement;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -8,8 +10,8 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class activityIndicator extends ItemStack {
-    public activityIndicator(OfflinePlayer player){
+public class ActivityIndicator extends ItemStack {
+    public ActivityIndicator(OfflinePlayer player){
         setAmount(1);
         setType(Material.PAPER);
         ItemMeta meta = getItemMeta();
@@ -18,7 +20,10 @@ public class activityIndicator extends ItemStack {
             meta.displayName(Component.text("Currently Online", TextColor.fromHexString("#17fc32"))
                     .decoration(TextDecoration.ITALIC, false));
         }else{
-            meta.displayName(Component.text("Currently Offline", TextColor.fromHexString("#e80e0e"))
+            long lastOnline = ProfileStatement.getLast_Online(player);
+            int rawTime = (int) (System.currentTimeMillis()-lastOnline)/1000;
+            String text = String.format("Last seen %s ago.", MillisToTime.withWord(rawTime));
+            meta.displayName(Component.text(text, TextColor.fromHexString("#e80e0e"))
                     .decoration(TextDecoration.ITALIC, false));
         }
         setItemMeta(meta);
