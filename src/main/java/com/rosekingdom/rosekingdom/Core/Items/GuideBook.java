20cd31e164b69dd1cba051eb8cd91bb8f2bf7d40
TMG <66220144+TMG8047KG@users.noreply.github.com>
2024-06-05 23:33:42 +0300

    Guide Book Info Update

--- src/main/java/com/rosekingdom/rosekingdom/Core/Items/GuideBook.java
+++ src/main/java/com/rosekingdom/rosekingdom/Core/Items/GuideBook.java
@@ -3,8 +3,10 @@ package com.rosekingdom.rosekingdom.Core.Items;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.WritableBookMeta;
+
+import java.util.List;
 
 public class GuideBook extends ItemStack {
     public GuideBook() {
@@ -12,9 +14,24 @@ public class GuideBook extends ItemStack {
         setAmount(1);
         ItemMeta meta = this.getItemMeta();
         meta.displayName(Component.text("Server Guide"));
-        WritableBookMeta pages = (WritableBookMeta) meta;
-
-        this.setItemMeta(meta);
-
+        BookMeta book = (BookMeta) meta;
+        book.setAuthor("RoseKing");
+        List<Component> pages = book.pages();
+        pages.add(Component.text(
+                "          Welcome\n" +
+                        "              to\n" +
+                        "       RoseKingdom\n" +
+                        "\n" +
+                        "===================\n" +
+                        "\n" +
+                        "  That is our server guide, where you can find all the needed information about everything we have made so far. \n" +
+                        "\n" +
+                        "==================="));
+        pages.add(Component.text(
+                "        Contents:\n" +
+                "1. Rules\n" +
+                "2. Features\n" +
+                "3. Credits"));
+        setItemMeta(book);
     }
 }
