1a117c47309450ea5e750d35aa9b65d96a22b580
TMG <66220144+TMG8047KG@users.noreply.github.com>
2024-06-26 00:29:29 +0300

    Builders Experiment Progress

---
+++ src/main/java/com/rosekingdom/rosekingdom/Core/Utils/GUIBuilder.java
@@ -0,0 +1,13 @@
+package com.rosekingdom.rosekingdom.Core.Utils;
+
+
+import java.util.Map;
+
+public class GUIBuilder {
+    private String title;
+    private Map<Integer, Item> selected;
+
+    public GUIBuilder title(String title) {
+        return this;
+    }
+}
