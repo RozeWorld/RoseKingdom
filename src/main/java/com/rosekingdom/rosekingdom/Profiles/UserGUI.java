7c3790dc182ae64ed916d3dc85c573de590c1d91
TMG <alex800kill@gmail.com>
2024-07-31 14:01:57 +0200

    Fixed Profiles Head now showing properly

--- src/main/java/com/rosekingdom/rosekingdom/Profiles/UserGUI.java
+++ src/main/java/com/rosekingdom/rosekingdom/Profiles/UserGUI.java
@@ -4,16 +4,19 @@ import com.rosekingdom.rosekingdom.Core.Items.InvsibleItem;
 import com.rosekingdom.rosekingdom.Core.gui.InventoryButton;
 import com.rosekingdom.rosekingdom.Core.gui.InventoryGUI;
 import com.rosekingdom.rosekingdom.Profiles.Items.ActivityIndicator;
-import com.rosekingdom.rosekingdom.Profiles.Items.PlayerHead;
 import com.rosekingdom.rosekingdom.Profiles.Items.ProfilePlayTime;
 import com.rosekingdom.rosekingdom.Profiles.Items.StreakScore;
+import com.rosekingdom.rosekingdom.RoseKingdom;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 public class UserGUI extends InventoryGUI {
 
@@ -34,7 +37,21 @@ public class UserGUI extends InventoryGUI {
         for(int i : playerHead){
             this.addButton(i, createButton(new InvsibleItem(Component.text(target.getName(), TextColor.fromHexString("#9c9c9c")))));
         }
-        this.addButton(10, createButton(new PlayerHead(target)));
+
+
+        var profile = Bukkit.createProfile(target.getUniqueId());
+        profile.update().thenAcceptAsync(
+                updatedProfile -> {
+                    var head = ItemStack.of(Material.PLAYER_HEAD);
+                    var meta = (SkullMeta) head.getItemMeta();
+                    meta.displayName(Component.text(player.getName(), TextColor.fromHexString("#9c9c9c")).decoration(TextDecoration.ITALIC, false));
+                    meta.setCustomModelData(2000);
+                    meta.setPlayerProfile(updatedProfile);
+                    head.setItemMeta(meta);
+                    this.addButton(10, createButton(head));
+                    super.decorate(player);
+                },
+                runnable -> Bukkit. getScheduler().runTask(RoseKingdom.getPlugin(RoseKingdom.class), runnable));
         this.addButton(21, createButton(new ProfilePlayTime(target)));
         this.addButton(8, createButton(new ActivityIndicator(target)));
         this.addButton(22, createButton(new StreakScore(target)));
